40e6ad6d78b466e83c99216910755c6daa5a465d
tuki9ko <arkmisha@gmail.com>
2022-12-10 21:33:53 +0900

    usersテーブルにnameを追加

--- server/database/migrations/000002_users.up.sql
+++ server/database/migrations/000002_users.up.sql
@@ -1,8 +1,10 @@
 CREATE TABLE users (
 	id SERIAL NOT NULL,
+	name VARCHAR(255) NOT NULL,
 	email VARCHAR(255) NOT NULL,
 	hashed_password VARCHAR(255) NOT NULL,
 	role_id INTEGER NOT NULL,
 	biography VARCHAR(255),
-	user_icon VARCHAR(255)
+	user_icon VARCHAR(255),
+	PRIMARY KEY (id)
 );
