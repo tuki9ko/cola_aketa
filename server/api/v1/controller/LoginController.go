872880ae43cc6feec3a0698ffcc48603a2862108
tuki9ko <arkmisha@gmail.com>
2022-12-11 21:21:21 +0900

    とりあえずログイン機能作成

--- server/api/v1/controller/LoginController.go
+++ server/api/v1/controller/LoginController.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/tuki9ko/cola_aketa/api/v1/service"
 )
@@ -17,11 +19,22 @@ func (lc LoginController) GetLogin(c *gin.Context) {
 
 func (lc LoginController) PostLogin(c *gin.Context) {
 	userId := c.PostForm("userId")
+	password := c.PostForm("password")
 
-	ls.Login(c, userId)
+	user, err := ls.Login(c, userId, password)
+
+	msg := ""
+
+	if user != nil {
+		msg = fmt.Sprintf("Login successful, hello %s.", user.Name)
+	}
+
+	if err != nil {
+		msg = err.Error()
+	}
 
 	c.JSON(200, gin.H{
-		"message": "login successful",
+		"message": msg,
 	})
 }
 
--- server/api/v1/service/LoginService.go
+++ server/api/v1/service/LoginService.go
@@ -1,16 +1,42 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/tuki9ko/cola_aketa/database"
+	"github.com/tuki9ko/cola_aketa/models"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginService struct{}
 
-func (s LoginService) Login(c *gin.Context, userId string) {
+func (s LoginService) Login(c *gin.Context, userId string, password string) (*models.User, error) {
+	user, err := models.Users(
+		qm.Where("email = ?", userId),
+	).One(c, database.Db)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if user == nil {
+		return nil, errors.New("Incorrect username or password")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.HashedPassword), []byte(password))
+
+	if err != nil {
+		return nil, errors.New("Incorrect username or password")
+	}
+
 	session := sessions.Default(c)
 	session.Set("userId", userId)
 	session.Save()
+
+	return user, nil
 }
 
 func (s LoginService) Logout(c *gin.Context) {
