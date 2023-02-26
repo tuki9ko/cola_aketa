ff7cec6ba3982c2c11db42b8e775a3bfab0b4fa2
tuki9ko <arkmisha@gmail.com>
2023-02-26 22:17:27 +0900

    パラメータ名をlowerCamelに変更

--- server/api/request/PostLoginParameter.go
+++ server/api/request/PostLoginParameter.go
@@ -1,6 +1,6 @@
 package request
 
 type PostLoginRequestParameter struct {
-	UserId   string `form:"user_id" json:"user_id" binding:"required,email"`
+	UserId   string `form:"userId" json:"userId" binding:"required,email"`
 	Password string `form:"password" json:"password" binding:"required"`
 }
--- server/api/request/PostSignupParameter.go
+++ server/api/request/PostSignupParameter.go
@@ -5,5 +5,5 @@ type PostSignupParameter struct {
 	Email     string `form:"email" json:"email" binding:"required,email,max=255"`
 	Password  string `form:"password" json:"password" binding:"required,min=8,max=255"`
 	Biography string `form:"biography" json:"biography" binding:"max=255"`
-	UserIcon  string `form:"user_icon" json:"user_icon" binding:"max=255"`
+	UserIcon  string `form:"userIcon" json:"userIcon" binding:"max=255"`
 }
